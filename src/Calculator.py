80e8874283d27bd7295b5b8e1c52d066322f3bf5
curiousbasil <faisukkar@gmail.com>
2019-10-28 00:09:50 -0400

    Task: Added variable testing , Ran 2nd successful test

--- src/Calculator.py
+++ src/Calculator.py
@@ -4,5 +4,7 @@ class Calculator:
     result = 0
 
     def __init__(self):
+        x = 2 + 2
+        self.result= x;
         pass
 
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -9,6 +9,9 @@ class MyTestCase(unittest.TestCase):
         self.assertIsInstance(calculator, Calculator)
 
 
+    def test_results_property_calculator(self):
+        calculator  = Calculator()
+        self.assertEqual(calculator.result, 4)
 
 if __name__ == '__main__':
     unittest.main()
